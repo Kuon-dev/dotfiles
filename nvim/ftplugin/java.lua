8850e10b3a5899f291fae960c29fa27541e6a06b
AaronLYR <83005177+Kuon-dev@users.noreply.github.com>
2022-09-27 16:04:55 +0800

    Create java.lua

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,78 @@
+if not vim.g.loaded_jdtls then
+  vim.g.loaded_jdtls = true
+  require("packer").loader "nvim-jdtls"
+end
+
+local jdtls = require "jdtls"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = "C:/Users/User/AppData/Local/cache/jdtls-workspace" .. project_name
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    "java", -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xms1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+
+    "-jar",
+    "C:/Users/User/AppData/Local/nvim-data/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+
+    "-configuration",
+    "C:/Users/User/AppData/Local/nvim-data/mason/packages/jdtls/config_win",
+
+    "-data", vim.fn.expand('C:Users/User/AppData/Local/cache/jdtls-workspace') .. project_name,
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = jdtls.setup.find_root { ".git", "mvnw", "gradlew", "pom.xml" },
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = { signatureHelp = { enabled = true } },
+  },
+
+  init_options = {
+    bundles = {},
+    extendedClientCapabilities = extendedClientCapabilities,
+  },
+
+  on_attach = function(client, bufnr)
+    jdtls.setup.add_commands()
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+
+    -- Mappings
+    -- Load mappings of lspconfig
+    require("core.utils").load_mappings("lspconfig", { buffer = bufnr })
+
+    -- nvim-jdtls specific
+    local opts = { noremap = true, silent = true }
+    buf_set_keymap("n", "<leader>di", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+    buf_set_keymap("n", "<leader>dt", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
+    buf_set_keymap("n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
+    buf_set_keymap("v", "<leader>de", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+    buf_set_keymap("n", "<leader>de", "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+    buf_set_keymap("v", "<leader>dm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+  end,
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+jdtls.start_or_attach(config)
